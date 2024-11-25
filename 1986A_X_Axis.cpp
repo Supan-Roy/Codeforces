d7a1cb0b4558663d22469bf7cf2f3aa36601d361
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2024-11-25 20:21:57 +0600

    Add files via upload

---
+++ 1986A_X_Axis.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int t;
+        cin >> t;
+        while (t--)
+        {
+                int x1, x2, x3;
+                cin >> x1 >> x2 >> x3;
+
+                int arr[] = {x1, x2, x3};
+                sort(arr, arr + 3);
+
+                int a = arr[1];
+
+                int result = abs(a - arr[0]) + abs(a - arr[1]) + abs(a - arr[2]);
+                cout << result << endl;
+        }
+        return 0;
+}
