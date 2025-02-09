d0defbd4ba5fb30d78f349d18d88c2d03e94bb2f
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-02-09 21:07:40 +0600

    Add files via upload

---
+++ 2065A_Skibidus_and_Amogu.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        string w;
+        cin>>w;
+
+        for(int i=0;i<w.size()-2;i++)
+        {
+                cout<<w[i];
+        }
+        cout<<"i"<<endl;
+    }
+    return 0;
+}
