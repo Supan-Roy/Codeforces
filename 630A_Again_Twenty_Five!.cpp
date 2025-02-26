efbb3883d2a214f1b708638b3a7dfb133e582caa
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-02-26 19:34:54 +0600

    Add files via upload

---
+++ 630A_Again_Twenty_Five!.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long n;
+    cin>>n;
+
+    if(n>=2){
+        cout<<"25"<<endl;
+    }
+    else {
+        cout<<"05"<<endl;
+    }
+    return 0;
+}
