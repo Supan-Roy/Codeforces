1d10fe9060fcdca504ac8159edaf1bb9db251e0f
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-05-26 21:14:16 +0600

    SR commit

---
+++ 2114A_Square_Year.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+            string s;
+            cin >> s;
+            int year = stoi(s);
+            bool found = false;
+
+            for (int a = 0; a <= 99 && !found; ++a)
+            {
+                    for (int b = 0; b <= 99; ++b)
+                    {
+                            int sum = a + b;
+                            if (sum * sum == year)
+                            {
+                                    cout << a << " " << b << endl;
+                                    found = true;
+                                    break;
+                            }
+                    }
+            }
+
+            if (!found)
+                    cout << -1 << endl;
+    }
+    return 0;
+}
