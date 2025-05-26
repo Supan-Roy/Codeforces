57953c20a6e9dce336b5a0e3714db3aaf52d666a
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-05-26 21:15:43 +0600

    Update 2114A_Square_Year.cpp

--- 2114A_Square_Year.cpp
+++ 2114A_Square_Year.cpp
@@ -9,26 +9,26 @@ int main()
     while (t--)
     {
             string s;
-            cin >> s;
-            int year = stoi(s);
-            bool found = false;
+            cin>>s;
+            int year=stoi(s);
+            bool found=false;
 
-            for (int a = 0; a <= 99 && !found; ++a)
+            for (int a=0;a<=99 && !found;a++)
             {
-                    for (int b = 0; b <= 99; ++b)
+                    for (int b=0;b<=99;b++)
                     {
-                            int sum = a + b;
-                            if (sum * sum == year)
+                            int sum=a+b;
+                            if (sum*sum==year)
                             {
-                                    cout << a << " " << b << endl;
-                                    found = true;
+                                    cout<<a<<" "<<b<<endl;
+                                    found=true;
                                     break;
                             }
                     }
             }
 
             if (!found)
-                    cout << -1 << endl;
+                    cout<<-1<<endl;
     }
     return 0;
 }
