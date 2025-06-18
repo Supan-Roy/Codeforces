76f98f3b6e1e6bd00e073c9a9ccb675ab0539c3d
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-06-19 00:18:22 +0600

    Add files via upload

---
+++ 112A_Petya_and_Strings.py
@@ -0,0 +1,12 @@
+text1 = input()
+text2 = input()
+
+text1=text1.lower()
+text2=text2.lower()
+
+if text1<text2:
+        print("-1")
+elif text1==text2:
+        print(0)
+elif text1>text2:
+        print('1')
