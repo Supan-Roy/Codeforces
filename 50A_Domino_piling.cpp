fd93164d89b1e0536fce76afffbdf909f86ce602
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-06-26 00:08:48 +0600

    Initial commit

---
+++ 50A_Domino_piling.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int M, N;
+    cin>>M>>N;
+    int sum=0;
+    sum = (M*N)/2;
+    cout<<sum<<endl;
+    return 0;
+}
