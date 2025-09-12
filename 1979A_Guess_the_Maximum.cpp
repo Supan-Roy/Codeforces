43b197dfdd61200187b4d3a6a661cb31d69cf600
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-09-12 20:26:42 +0600

    Initial commit

---
+++ 1979A_Guess_the_Maximum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+                cin>>a[i];
+        }
+        int ans = INT_MAX;
+        for(int i=0;i<n-1;i++){
+                ans=min(ans, max(a[i], a[i+1]));
+        }
+        cout<<ans-1<<endl;
+    }
+    return 0;
+}
