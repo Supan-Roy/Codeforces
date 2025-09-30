1cbaa70eddcc5e60d34ced47a31e482caec0034a
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-09-30 10:21:02 +0600

    Initial commit

---
+++ 263A_Beautiful_Matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int r, c;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+                int x;
+                cin>>x;
+                if(x==1){
+                        r=i;
+                        c=j;
+                }
+        }
+    }
+    int ans=abs(r-3)+abs(c-3);
+    cout<<ans<<endl;
+    return 0;
+}
