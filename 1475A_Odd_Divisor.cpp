4cc19fe6058d72a842b92cc4a1215c6a72fd488f
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-10-01 11:38:10 +0600

    Initial commit

---
+++ 1475A_Odd_Divisor.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        while(n%2==0){
+                n/=2;
+        }
+        if(n>1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
