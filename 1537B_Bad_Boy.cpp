1ed4e7f7f07d69237c85e65bbd975362f18f3eba
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-10-07 19:42:15 +0600

    Initial commit

---
+++ 1537B_Bad_Boy.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m, i, j;
+        cin>>n>>m>>i>>j;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+    }
+    return 0;
+}
